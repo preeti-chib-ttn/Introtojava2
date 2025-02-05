954fcea4421f5b529e32ca4678da8febfa48c27d
Preeti Chib <preeti.chib@tothenew.com>
2025-02-05 13:17:22 +0530

    Added Q1

--- src/Question1.java
+++
@@ -1,9 +0,0 @@
-/*Q1. Write a program to display values of enums
-using a constructor & getPrice() method
-(Example display house & their prices)*/
-
-public class Question1 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ src/Question1/Question1.java
@@ -0,0 +1,27 @@
+package Question1;
+/*Q1. Write a program to display values of enums
+using a constructor & getPrice() method
+(Example display house & their prices)*/
+enum Houses{
+    SEAFACING(100000),
+    COTTAGE(10000),
+    VILLA(232000);
+
+    private final int price;
+    Houses(int price){
+        this.price=price;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+}
+public class Question1 {
+    public static void main(String[] args) {
+
+        for(Houses h: Houses.values()){
+            System.out.println("The price of "+h+" is: "+h.getPrice());
+        }
+    }
+}
